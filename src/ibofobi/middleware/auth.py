5659e2e29a28010304be1a4ea56744a72ccbdec7
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-09-09 19:22:34 +0000

    Add auth middleware.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1361 5646265b-94b7-0310-9681-9501d24b2df7

---
+++ src/ibofobi/middleware/auth.py
@@ -0,0 +1,17 @@
+from django.middleware import admin
+
+class AdminUserRequired(admin.AdminUserRequired):
+    """Admin middleware. If a view-function has the admin_required
+    property, access will be granted only to valid users with the
+    "is_staff" flag set."""
+
+    def process_view(self, request, view_func, param_dict):
+        """Make sure the user is logged in and is a valid admin user
+        before allowing any access to views with the admin_required
+        property."""
+
+        if getattr(view_func, 'admin_required'):
+            return super(self, admin.AdminUserRequired).process_view(request, view, param_dict)
+        else:
+            return view_func
+
