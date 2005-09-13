a7de7f67058ab549cdf1dfe5b0fbde21411281ec
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-09-13 12:09:29 +0000

    Fix bug in setting of Vary-header.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1382 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/middleware/xhtml.py
+++ src/ibofobi/middleware/xhtml.py
@@ -2,6 +2,7 @@ import re
 
 re_ct_xhtml = re.compile(r'^application/xhtml\+xml\b')
 re_accept_xhtml = re.compile(r'\bapplication/xhtml\+xml\b')
+re_vary_accept = re.compile(r'\bAccept(?!-)\b')
 
 class XHTMLAsHTMLMiddleware:
     """I change content-type application/xhtml+xml into text/html, if the
@@ -9,15 +10,16 @@ class XHTMLAsHTMLMiddleware:
 
     def process_response(self, request, response):
         if re_ct_xhtml.match(response['Content-Type']):
+            if response.has_header('Vary'):
+                if not re_vary_accept.search(response['Vary']):
+                    response['Vary'] = response['Vary'] + ', Accept'
+            else:
+                response['Vary'] = 'Accept'
+
             accept = request.META.get('HTTP_ACCEPT', '')
             if not re_accept_xhtml.search(accept):
                 ct = response['Content-Type']
                 ct = ct.replace('application/xhtml+xml', 'text/html')
                 response['Content-Type'] = ct
-
-            if response.has_header('Vary'):
-                response['Vary'] = response['Vary'] + ', Accept'
-            else:
-                response['Vary'] = 'Accept'
 
         return response
