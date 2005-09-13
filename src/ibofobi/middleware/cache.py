62e18201d7588419d5ffecdbd474a64699f072bf
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-09-13 19:57:12 +0000

    Add incomplete cache middleware.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1384 5646265b-94b7-0310-9681-9501d24b2df7

---
+++ src/ibofobi/middleware/cache.py
@@ -0,0 +1,81 @@
+"""A Vary-capable cache middleware. Adapted from Djangos cache
+middleware."""
+
+from django.conf import settings
+from django.core.cache import cache
+from django.utils.httpwrappers import HttpResponseNotModified
+import datetime, md5
+
+class CacheMiddleware:
+    """
+    Cache middleware. If this is enabled, each Django-powered page will be
+    cached for CACHE_MIDDLEWARE_SECONDS seconds. Cache is based on URLs. Pages
+    with GET or POST parameters are not cached.
+
+    Pages will be cached based on the contents of the request headers
+    listed in the response Vary-header [ FIXME -- need example, or better
+    description? ]. This means that pages cannot change their Vary-header,
+    without strange results.
+
+    If the cache is shared across multiple sites using the same Django
+    installation, set the CACHE_MIDDLEWARE_KEY_PREFIX to the name of the
+    site, or some other string that is unique to this Django instance, to
+    prevent key collisions.
+    """
+    def process_request(self, request):
+        """Checks whether the page is already cached. If it is, returns
+        the cached version."""
+
+        if request.GET or request.POST:
+            request._cache_middleware_set_cache = False
+            return None # Don't bother checking the cache.
+
+        vary_key = 'ibofobi.cache.vary.%s.%s' % \
+            (settings.CACHE_MIDDLEWARE_KEY_PREFIX, request.path)
+        request._ibofobi_cache_vary_key = vary_key
+
+        page_key = 'ibofobi.cache.page.%s.%s' % \
+            (settings.CACHE_MIDDLEWARE_KEY_PREFIX, request.path)
+        request._ibofobi_cache_page_key = page_key
+
+        request._ibofobi_cache_varies = varies = cache.get(vary_key, None)
+        response = None
+
+        if varies:
+            response_key = page_key
+            for meta_key in varies:
+                response_key = response_key + '.' + request.META.get(meta_key, '')
+            request._ibofobi_cache_response_key = response_key
+            response = cache.get(response_key, None)
+        
+        if response is None:
+            request._ibofobi_cache_set = True
+            return None
+        else:
+            request._cache_middleware_set_cache = False
+            return response
+
+    def process_response(self, request, response):
+        """Sets the cache, if needed."""
+        if request._cache_middleware_set_cache:
+            #response['ETag'] = md5.new(content).hexdigest()
+            #response['Last-Modified'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+            if request._ibofobi_cache_varies:
+                response_key = request._ibofobi_cache_response_key
+            else:
+                varies = []
+                response_key = request._ibofobi_cache_page_key
+                for header in response.get('Vary', '').split(','):
+                    header = header.strip()
+                    meta_key = 'HTTP_' + header.upper().replace('-', '_')
+                    response_key = response_key + '.' + request.META.get(meta_key, '')
+                    varies.append(meta_key)
+
+                cache.set(request._ibofobi_cache_vary_key, meta_keys,
+                          settings.CACHE_MIDDLEWARE_SECONDS)
+
+            cache.set(response_key, response,
+                      settings.CACHE_MIDDLEWARE_SECONDS)
+
+        return response
