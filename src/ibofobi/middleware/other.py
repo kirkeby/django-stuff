8cd61c58c4f7eab7337edadba55607bea697c58e
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-09-13 20:31:47 +0000

    Add FixVaryHeader middleware.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1385 5646265b-94b7-0310-9681-9501d24b2df7

---
+++ src/ibofobi/middleware/other.py
@@ -0,0 +1,12 @@
+import re
+
+re_vary_cookie = re.compile(r'\bCookie\b')
+
+class FixVaryHeader:
+    def process_response(self, request, response):
+        if response.has_header('Vary'):
+            if not re_vary_cookie.search(response['Vary']):
+                response['Vary'] = response['Vary'] + ', Cookie'
+        else:
+            response['Vary'] = 'Cookie'
+        return response
