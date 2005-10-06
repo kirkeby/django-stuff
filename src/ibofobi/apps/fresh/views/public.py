bb6c6a0edc1064f032edf6f040f9a54d8504c224
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-10-06 13:20:54 +0000

    Skip logging page-view if settings.FRESH_SKIP.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1428 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/apps/fresh/views/public.py
+++ src/ibofobi/apps/fresh/views/public.py
@@ -1,7 +1,12 @@
 from django.models.fresh import pageviews
 from django.utils.httpwrappers import HttpResponse
 
+from django.conf import settings
+
 def fresh(request):
+    if getattr(settings, 'FRESH_SKIP', False):
+        return
+
     if request.META.has_key('HTTP_X_FORWARDED_FOR'):
         ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
     elif request.META.has_key('REMOTE_ADDR'):
