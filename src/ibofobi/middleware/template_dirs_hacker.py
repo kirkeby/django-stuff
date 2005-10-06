e4ae7c7e63df42963ca9786974c9266db268c736
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-10-06 06:55:36 +0000

    Save value of settings.TEMPLATE_DIRS and append to all hacked TEMPLATE_DIRS; this is good with ADD_APP_TEMPLATE_DIRS.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1419 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/middleware/template_dirs_hacker.py
+++ src/ibofobi/middleware/template_dirs_hacker.py
@@ -5,10 +5,15 @@ class TemplateDirsHacker:
     URL I munge django.conf.settings.TEMPLATE_DIRS! Beware of my l33t
     eviln3ss! *Hrm* Sorry."""
 
+    def __init__(self):
+        self.app_templates = []
+        self.app_templates.extend(settings.TEMPLATE_DIRS)
+
     def process_request(self, request):
         for prefix, template_dirs in settings.TEMPLATE_DIRS_MAPPING:
             if request.path.startswith(prefix):
                 settings.TEMPLATE_DIRS[:] = template_dirs[:]
+                settings.TEMPLATE_DIRS.extend(self.app_templates)
                 break
         else:
             raise AssertionError, 'no TEMPLATE_DIRS found'
