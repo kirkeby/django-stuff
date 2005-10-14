2a3d8e11cd3e5fa8f9e46ccac03ed08dce590702
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-10-14 09:14:24 +0000

    r2770@moria:  sune | 2005-10-14 11:13:50 +0200  Fresh: Add pageview.js.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1442 5646265b-94b7-0310-9681-9501d24b2df7

---
+++ src/ibofobi/apps/fresh/media/fresh/pageview.js
@@ -0,0 +1,23 @@
+(function () {
+    var req = false;
+    try {
+        req = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            req = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (f) {
+            req = false;
+        }
+    }
+    if(! req && typeof XMLHttpRequest != 'undefined') {
+        req = new XMLHttpRequest();
+    }
+
+    if(req) {
+        var path = '/fresh/?url=' + document.location;
+        path = path + '&ref=' + document.referrer;
+        path = path + '&qux=' + (new Date()).getTime();
+        req.open("GET", path, true);
+        req.send("");
+    }
+)();
