757627c2d5a53718a8a5f70a680fbcc9c1df8a92
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-10-18 07:04:29 +0000

    r2843@moria:  sune | 2005-10-18 09:04:24 +0200  Return a HTTPResponse-object even when not logging page-views.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1449 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/apps/fresh/views/public.py
+++ src/ibofobi/apps/fresh/views/public.py
@@ -5,22 +5,23 @@ from django.conf import settings
 
 def fresh(request):
     if getattr(settings, 'FRESH_SKIP', False):
-        return
+        pass
 
-    if request.META.has_key('HTTP_X_FORWARDED_FOR'):
-        ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
-    elif request.META.has_key('REMOTE_ADDR'):
-        ip = request.META['REMOTE_ADDR']
     else:
-        ip = '0.0.0.0'
+        if request.META.has_key('HTTP_X_FORWARDED_FOR'):
+            ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
+        elif request.META.has_key('REMOTE_ADDR'):
+            ip = request.META['REMOTE_ADDR']
+        else:
+            ip = '0.0.0.0'
 
-    url = '/' + request.GET['url'].split('/', 3)[-1]
+        url = '/' + request.GET['url'].split('/', 3)[-1]
 
-    pv = pageviews.PageView(url=url)
-    pv.referrer = request.GET.get('ref', '') or None
-    pv.user_agent = request.META.get('HTTP_USER_AGENT', None)
-    pv.ip_address = ip
-    pv.save()
+        pv = pageviews.PageView(url=url)
+        pv.referrer = request.GET.get('ref', '') or None
+        pv.user_agent = request.META.get('HTTP_USER_AGENT', None)
+        pv.ip_address = ip
+        pv.save()
 
     r = HttpResponse('/* Intentionally left empty */', 'text/css')
     r['Cache-Control'] = 'max-age=0'
