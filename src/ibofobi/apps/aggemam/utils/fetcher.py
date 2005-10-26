12fe2df04daacebcb241ccf321c63ba90e8d73ee
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-10-26 18:02:21 +0000

    * Aggemam fetcher now follows 302 temporary redirects.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1493 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/apps/aggemam/utils/fetcher.py
+++ src/ibofobi/apps/aggemam/utils/fetcher.py
@@ -1,12 +1,11 @@
 import urlparse
 import httplib
 
+AGGEMAM_MAX_REDIRECT_DEPTH = 3
+
 class Fetcher:
     def __init__(self, url):
         self.url = url
-
-        self.scheme, self.netloc, self.path, self.params, self.query, self.fragment = \
-            urlparse.urlparse(self.url)
 
         self.request_headers = {
             'User-Agent': 'Aggemam (+http://ibofobi.dk/stuff/aggemam/)',
@@ -14,13 +13,33 @@ class Fetcher:
         self.status = self.reason = self.headers = self.body = None
     
     def fetch(self):
-        c = httplib.HTTPConnection(self.netloc)
-        c.request('GET', self.path + '?' + self.query,
-                  headers=self.request_headers)
-        r = c.getresponse()
+        r, b = self.__fetch(self.url, 0)
+
         self.status = r.status
         self.reason = r.reason
         self.headers = ''.join(r.msg.headers)
         self.parsed = r.msg
-        self.body = r.read()
+        self.body = b
+
+    def __fetch(self, url, redirect_depth):
+        if redirect_depth == AGGEMAM_MAX_REDIRECT_DEPTH:
+            raise AssertionError, 'maximum redirect depth hit'
+
+        scheme, netloc, path, params, query, fragment = urlparse.urlparse(url)
+
+        if not scheme == 'http':
+            raise AssertionError, 'unknown scheme: %s' % url
+
+        c = httplib.HTTPConnection(netloc)
+        c.request('GET', path + '?' + query, headers=self.request_headers)
+        r = c.getresponse()
+        body = r.read()
         c.close()
+
+        if r.status == 302:
+            # Handle "302 Found" (i.e. temporarily moved),
+            url = r.msg['location']
+            return self.__fetch(url, redirect_depth + 1)
+
+        else:
+            return r, body
