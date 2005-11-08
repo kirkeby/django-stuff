9fc2e11d30a2d3183bca8ee42067199bced43091
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-11-08 20:54:19 +0000

    r3348@moria:  sune | 2005-11-08 18:54:07 +0100  Remove urls.public module, just use urls.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1506 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/apps/aggemam/urls/public.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('ibofobi.apps.aggemam.views.public',
-    (r'^$', 'index'),
-    (r'^subscriptions/$', 'list_subscriptions'),
-    (r'^unread-posts/$', 'list_unread_posts'),
-)
