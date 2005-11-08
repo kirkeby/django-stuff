ba65b453b60602570678436ed3eb5a55dcd6f5f6
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-11-08 22:07:40 +0000

    r3362@moria:  sune | 2005-11-08 23:07:32 +0100  Test: Fix yaml to work as a non top-level module; remove unused redump module.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1513 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/utils/test/yaml/__init__.py
+++ src/ibofobi/utils/test/yaml/__init__.py
@@ -3,9 +3,6 @@ from load import loadFile, load, Parser, l
 from dump import dump, dumpToFile, Dumper, d
 from stream import YamlLoaderException, StringStream, FileStream
 from timestamp import timestamp
-import sys
-if sys.hexversion >= 0x02020000:
-    from redump import loadOrdered
 
 try:
     from ypath import ypath
@@ -13,5 +10,6 @@ except NameError:
     def ypath(expr,target='',cntx=''):
         raise NotImplementedError("ypath requires Python 2.2")
 
+import sys
 if sys.hexversion < 0x02010000:
     raise 'YAML is not tested for pre-2.1 versions of Python'
