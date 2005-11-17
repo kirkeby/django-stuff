d575e798bd51cf5c0fd93b4373a824d896dbf0e5
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-11-17 14:13:20 +0000

    Add tests for add_userpostmark.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/ibofobi@1518 5646265b-94b7-0310-9681-9501d24b2df7

--- src/ibofobi/apps/aggemam/tests/__init__.py
+++ src/ibofobi/apps/aggemam/tests/__init__.py
@@ -1,8 +1,14 @@
 """
+>>> from django.models.auth import users
+>>> user = users.create_user('me', 'John', 'Doe')
+>>> user.save()
+
 >>> first_post.title
 'First Post!'
 >>> first_post.fetched is None
 False
 >>> first_post.get_preferred_link().href
 'http://example.com/post/'
+
+>>> first_post.add_userpostmark(user, 'rd')
 """
